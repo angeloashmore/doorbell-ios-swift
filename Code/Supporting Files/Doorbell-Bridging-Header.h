b6a88ac460c276f88d3dd2a04de8a72e85e1e94f
Angelo Ashmore <angeloashmore@me.com>
2015-05-23 13:16:30 -1000

    Manually adding LayerKit and Atlas

--- Code/Supporting Files/Doorbell-Bridging-Header.h
+++ Code/Supporting Files/Doorbell-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#import <LayerKit/LayerKit.h>
+// #import <LayerKit/LayerKit.h>
 #import <Bolts/Bolts.h>
 #import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
